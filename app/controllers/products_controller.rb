5c6a8778f6a77383136e42e97996e7433f14ca20
Nick Gibb <gibbnick@gmail.com>
2025-09-24 18:06:02 +0200

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  # has_rich_text :description
+  # validates :name, presence: true
+
+  allow_unauthenticated_access only: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.expect(product: [ :name, :description ])
+    end
+end
